b66ca33e1b56afafa96cc7e3ecbaecfde3590296
Mohammad <67665004+mohammadhlm@users.noreply.github.com>
2024-09-21 03:46:47 +0330

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+sudo rm /var/lib/apt/lists/lock
+sudo rm /var/cache/apt/archives/lock
+sudo rm /var/lib/dpkg/lock*
 sudo dpkg --configure -a
 sudo apt update && sudo apt upgrade -y
 sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential git make ncdu -y
