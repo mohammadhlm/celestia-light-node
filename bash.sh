568c34eb1059dcf73cdb19c549de1284763ac3f1
Mohammad <67665004+mohammadhlm@users.noreply.github.com>
2024-12-14 23:24:33 +0330

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -18,7 +18,7 @@ cd $HOME
 rm -rf celestia-node
 git clone https://github.com/celestiaorg/celestia-node.git
 cd celestia-node/
-git checkout tags/v0.16.0
+git checkout tags/v0.20.4
 make build
 make install
 make cel-key
